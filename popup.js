918e587745d286226fd303b66c923e3223df94dc
jacobmsewell <jacobmsewell94@gmail.com>
2022-02-02 15:58:16 -0800

    Add backend support for storing tab profiles

--- popup.js
+++ popup.js
@@ -1,24 +1,34 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
+
+    //chrome.storage.sync.clear();
+    chrome.tabs.query({currentWindow: true}, function(tabs){
+      let tabUrls = tabs.map(t => t.url)
+      let tabProfileName = document.getElementById('tab-profile-name').value;
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: storeTabProfile,
+        args: [tabUrls, tabProfileName]
+      });
+
     });
+
+
   });
-  
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-});
+
+function storeTabProfile(tabUrls, tabProfileName) {
+  chrome.storage.sync.get("tabProfiles", function(result) {
+
+    let updatedProfiles = !!result.tabProfiles ? result.tabProfiles : {};
+
+    updatedProfiles[tabProfileName] = tabUrls;
+
+
+    chrome.storage.sync.set({"tabProfiles": updatedProfiles}, function() {
+      chrome.storage.sync.get(["tabProfiles"], function(result) {
+        console.log("UPDATED PROFILE LIST!! ", result);
+      });
+    })
+  });
 }
