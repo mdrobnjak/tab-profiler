50ca791a584c111a7260170be69aadbb6e600971
Mark Drobnjak <mark.drobnjak@gmail.com>
2022-02-07 13:38:18 -0700

    Iterating on UI

--- popup.js
+++ popup.js
@@ -1,27 +1,30 @@
 
-//Use following icons: camera, cog, arrow-alt-circle-up (right?)
+// TODO: Use icons: camera, cog, arrow-alt-circle-right
 
 var body = document.getElementById('body');
 body.className = "body";
 var popup = document.createElement('div');
 popup.className = "popup";
+
+// Snapshots
 var snapshotButtons = document.createElement('div');
 
 var snapshots = getSnapshots();
 
-
 snapshots.forEach(snapshot => {
   var snapshotButton = document.createElement('button');
   snapshotButton.className = "btn";
-  snapshotButton.innerHTML = snapshot.Name;
+  snapshotButton.innerHTML = "<i class=\"fas fa-arrow-alt-circle-right\"></i> " + snapshot.Name;
   snapshotButtons.appendChild(snapshotButton);
 });
 
 popup.appendChild(snapshotButtons);
 
+// Capture
+
 var captureButton =  document.createElement('button');
 captureButton.className = "btn";
-captureButton.innerHTML = "Capture New Snapshot";
+captureButton.innerHTML = "<i class=\"fas fa-camera\"></i> " + "Capture New Snapshot";
 popup.appendChild(captureButton);
 
 body.appendChild(popup);
