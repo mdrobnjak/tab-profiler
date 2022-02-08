cb23a3e9573e0b7d2e24ca51d0db579bf78194b0
Mark Drobnjak <mark.drobnjak@gmail.com>
2022-02-07 17:53:14 -0700

    Prepared for Demo

--- options.js
+++ options.js
@@ -1,22 +1,30 @@
+
 $(document).ready(function () {
-  var snapshots = [
-    { "Name": "My Snapshot", "URL": "about:blank" }];
 
-  $("#jsGrid").jsGrid({
-    width: "100%",
-    height: "400px",
+  chrome.storage.local.get("tabSnapshots", function(result) {
 
-    inserting: true,
-    editing: true,
-    sorting: true,
-    paging: true,
+    var snapshots = !!result["tabSnapshots"] ? result["tabSnapshots"] : [];
+    snapshots.forEach(snapshot => {
+      snapshot.urlList = snapshot.urlList.join(",");
+    });
 
-    data: snapshots,
-
-    fields: [
-        { name: "Name", type: "text", width: 100, validate: "required" },
-        { name: "URLs", type: "text", sorting: false },
-        { type: "control" }
-    ]
+    $("#jsGrid").jsGrid({
+      width: "100%",
+      height: "400px",
+  
+      inserting: true,
+      editing: true,
+      sorting: true,
+      paging: true,
+  
+      data: snapshots,
+  
+      fields: [
+          { name: "name", type: "text", width: 100, validate: "required" },
+          { name: "urlList", type: "text", sorting: false },
+          { type: "control" }
+      ]
+    });
   });
+
 });
--- popup.js
+++ popup.js
@@ -1,37 +1,124 @@
 
-// TODO: Use icons: camera, cog, arrow-alt-circle-right
-
 var body = document.getElementById('body');
 body.className = "body";
 var popup = document.createElement('div');
 popup.className = "popup";
-
-// Snapshots
-var snapshotButtons = document.createElement('div');
-
-var snapshots = getSnapshots();
-
-snapshots.forEach(snapshot => {
-  var snapshotButton = document.createElement('button');
-  snapshotButton.className = "btn";
-  snapshotButton.innerHTML = "<i class=\"fas fa-arrow-alt-circle-right\"></i> " + snapshot.Name;
-  snapshotButtons.appendChild(snapshotButton);
-});
-
-popup.appendChild(snapshotButtons);
-
-// Capture
-
-var captureButton =  document.createElement('button');
-captureButton.className = "btn";
-captureButton.innerHTML = "<i class=\"fas fa-camera\"></i> " + "Capture New Snapshot";
-popup.appendChild(captureButton);
-
 body.appendChild(popup);
 
-function getSnapshots() {
-  return [
-    { "Name": "My Snapshot", "URL": "about:blank" },
-    { "Name": "My Snapshot2", "URL": "about:config" }
-  ]
+renderCaptureButton();
+
+renderConfigButton();
+
+renderClearButton();
+
+renderSnapshotButtons();
+
+/*
+  Capture
+*/
+function renderCaptureButton() {
+  var captureButton =  document.createElement('button');
+  captureButton.className = "btn";
+  captureButton.id = "capture";
+  captureButton.innerHTML = "<i class=\"fas fa-camera\"></i> " + "Capture";
+  popup.appendChild(captureButton);  
+}
+
+capture.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.tabs.query({currentWindow: true}, function(tabs){
+    let tabUrls = tabs.map(t => t.url)
+    let tabSnapshotName = tabs[0].title;
+    saveSnapshot(tabUrls, tabSnapshotName, renderSnapshotButtons());
+  });
+
+  window.location.reload();
+});
+
+function saveSnapshot(tabUrls, tabSnapshotName, callbackFn) {
+  chrome.storage.local.get("tabSnapshots", function(result) {
+
+    let updatedSnapshots = !!result.tabSnapshots ? result.tabSnapshots : [];
+    updatedSnapshots.push({
+      name: tabSnapshotName, 
+      urlList: tabUrls
+    });
+
+    chrome.storage.local.set({"tabSnapshots": updatedSnapshots}, callbackFn);
+  });
+}
+
+/*
+  Config
+*/
+function renderConfigButton() {
+  var configButton =  document.createElement('button');
+  configButton.className = "btn";
+  configButton.id = "config";
+  configButton.innerHTML = "<i class=\"fas fa-cog\"></i> " + "Config";
+  popup.appendChild(configButton);  
+}
+
+config.addEventListener("click", async () => {
+  chrome.tabs.create({
+    url: "chrome-extension://hoadnmjaaoigkneinkbihgdmcdphdidi/options.html"
+  });
+});
+
+/*
+  Clear
+*/
+function renderClearButton() {
+  var clearButton =  document.createElement('button');
+  clearButton.className = "btn";
+  clearButton.id = "clear";
+  clearButton.innerHTML = "<i class=\"fas fa-trash\"></i> " + "Clear";
+  popup.appendChild(clearButton);  
+}
+
+clear.addEventListener("click", async () => {
+  chrome.storage.local.clear();
+
+  window.location.reload();
+});
+
+/*
+  Load Snapshots
+*/
+function renderSnapshotButtons() {
+
+  var snapshotButtons = document.createElement('div');
+  popup.appendChild(snapshotButtons);
+
+  chrome.storage.local.get("tabSnapshots", function(result) {
+
+    var snapshots = !!result["tabSnapshots"] ? result["tabSnapshots"] : [];
+
+    snapshots.forEach(snapshot => {
+      var snapshotButton = document.createElement('button');
+      snapshotButtons.appendChild(snapshotButton);
+      snapshotButton.className = "btn";
+      snapshotButton.id = "snapshot";
+      snapshotButton.innerHTML = "<i class=\"far fa-arrow-alt-circle-right\"></i> " + snapshot.name;
+      snapshotButton.addEventListener("click", async () => loadSnapshot(snapshot.name));
+    });
+
+  });
+}
+
+function loadSnapshot(name) {
+  chrome.storage.local.get("tabSnapshots", function(result) {
+    result["tabSnapshots"].forEach(tabSnapshot => {
+      if(tabSnapshot.name == name) {
+        tabSnapshot.urlList.forEach(url => {
+          chrome.tabs.create({
+            url: url
+          });
+        })
+      }
+    })
+  })
+
+  window.location.reload();
 }
